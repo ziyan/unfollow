ff1da2a8db415ce161a13c4f8d4b66ac63f63f56
Ziyan Zhou <zhou@ziyan.info>
2014-12-07 18:29:14 -0800

    Do the more expensive call first.

--- appengine/unfollow/src/unfollow/network/utils.go
+++ appengine/unfollow/src/unfollow/network/utils.go
@@ -177,6 +177,27 @@ func DiscoverNodes(db *db.Database, ids []int64) ([]*models.Node, error) {
 func DiscoverNode(db *db.Database, id int64) (*models.Node, error) {
     t := twitter.New(db.Context, nil)
 
+    // discover recent friends and followers
+    friendsIDs, err := t.FriendsIDs(id)
+    if err != nil {
+        return nil, err
+    }
+
+    followersIDs, err := t.FollowersIDs(id)
+    if err != nil {
+        return nil, err
+    }
+
+    friends, err := t.Friends(id)
+    if err != nil {
+        return nil, err
+    }
+
+    followers, err := t.Followers(id)
+    if err != nil {
+        return nil, err
+    }
+
     user, err := t.User(id)
     if err != nil {
         return nil, err
@@ -190,29 +211,10 @@ func DiscoverNode(db *db.Database, id int64) (*models.Node, error) {
 
     node := TwitterUserToNode(user)
     node.SetKey(models.NodeKey(db, id))
-
-    node.FriendsIDs, err = t.FriendsIDs(id)
-    if err != nil {
-        return nil, err
-    }
-
-    node.FollowersIDs, err = t.FollowersIDs(id)
-    if err != nil {
-        return nil, err
-    }
+    node.FriendsIDs = friendsIDs
+    node.FollowersIDs = followersIDs
 
     if err := models.PutNode(db, node); err != nil {
-        return nil, err
-    }
-
-    // discover recent friends and followers
-    friends, err := t.Friends(id)
-    if err != nil {
-        return nil, err
-    }
-
-    followers, err := t.Followers(id)
-    if err != nil {
         return nil, err
     }
 
